41b1ee38e568164590a31278145b48a5bfa988c0
Mike Baldwin <mikebaldwin21@gmail.com>
2018-03-03 23:53:34 -0500

    New project, who dis.

---
+++ Assets/Asteroids/Scripts/Asteroid.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+	public class Asteroid : MonoBehaviour
+	{
+		private bool isWrappingOnX;
+		private bool isWrappingOnY;
+
+		private void Start()
+		{
+			Vector3 startPos = Camera.main.ViewportToWorldPoint(new Vector3(Random.value, Random.value));
+			transform.position = startPos;
+			GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle.normalized * 50);
+			GetComponent<Rigidbody2D>().AddTorque(3);
+		}
+
+		private void OnBecameInvisible()
+		{
+			Vector2 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+
+			if((viewportPos.x < 0 || viewportPos.x > 1) && !isWrappingOnX)
+			{
+				transform.position = new Vector2(-transform.position.x, transform.position.y);
+				isWrappingOnX = true;
+			}
+
+			if((viewportPos.y < 0 || viewportPos.y > 1) && !isWrappingOnY)
+			{
+				transform.position = new Vector2(transform.position.x, -transform.position.y);
+				isWrappingOnY = true;
+			}
+		}
+
+		private void OnBecameVisible()
+		{
+			isWrappingOnX = false;
+			isWrappingOnY = false;
+		}
+	}
+}
---
+++ Assets/Asteroids/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using UnityEngine;
+
+namespace Asteroids
+{
+	public class GameManager : MonoBehaviour
+	{
+		public GameObject asteroidPrefab;
+		public int numOfRoidsToSpawn = 10;
+
+		private void Awake()
+		{
+			for(int i = 0; i < numOfRoidsToSpawn; i++)
+			{
+				Instantiate(asteroidPrefab);
+			}
+		}
+	}
+}
