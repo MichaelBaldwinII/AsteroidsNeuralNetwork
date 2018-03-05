60d15df68cb9bf9236f91fe923feb25c1be87491
Mike Baldwin <mikebaldwin21@gmail.com>
2018-03-05 16:29:47 -0500

    Got multiGen working.

--- Assets/Asteroids/Scripts/Asteroid.cs
+++ Assets/Asteroids/Scripts/Asteroid.cs
@@ -5,72 +5,53 @@ namespace Asteroids
 	public class Asteroid : MonoBehaviour
 	{
 		public AsteroidSize size;
-		private bool isWrappingOnX;
-		private bool isWrappingOnY;
-		private AsteroidSpawner spawner;
-		private ScoreManager scoreManager;
-
-		private void Awake()
-		{
-			spawner = FindObjectOfType<AsteroidSpawner>();
-			scoreManager = FindObjectOfType<ScoreManager>();
-		}
 
 		private void OnBecameInvisible()
 		{
 			Vector2 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
 
-			if((viewportPos.x < 0 || viewportPos.x > 1) && !isWrappingOnX)
+			if(viewportPos.x < 0 || viewportPos.x > 1)
 			{
 				transform.position = new Vector2(-transform.position.x, transform.position.y);
-				isWrappingOnX = true;
 			}
 
-			if((viewportPos.y < 0 || viewportPos.y > 1) && !isWrappingOnY)
+			if(viewportPos.y < 0 || viewportPos.y > 1)
 			{
 				transform.position = new Vector2(transform.position.x, -transform.position.y);
-				isWrappingOnY = true;
 			}
-		}
-
-		private void OnBecameVisible()
-		{
-			isWrappingOnX = false;
-			isWrappingOnY = false;
 		}
 
 		private void OnCollisionEnter2D(Collision2D other)
 		{
-			//We split into two smaller asteroids of the next size down
 			switch(size)
 			{
 				case AsteroidSize.LARGE:
-					spawner.Spawn(transform.position, AsteroidSize.MEDIUM);
-					spawner.Spawn(transform.position, AsteroidSize.MEDIUM);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.MEDIUM);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.MEDIUM);
 					Destroy(gameObject);
-					scoreManager.currentScore += 50;
+					ScoreManager.Instance.currentScore += 50;
 					break;
 				case AsteroidSize.MEDIUM:
-					spawner.Spawn(transform.position, AsteroidSize.NORMAL);
-					spawner.Spawn(transform.position, AsteroidSize.NORMAL);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.NORMAL);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.NORMAL);
 					Destroy(gameObject);
-					scoreManager.currentScore += 40;
+					ScoreManager.Instance.currentScore += 40;
 					break;
 				case AsteroidSize.NORMAL:
-					spawner.Spawn(transform.position, AsteroidSize.SMALL);
-					spawner.Spawn(transform.position, AsteroidSize.SMALL);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.SMALL);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.SMALL);
 					Destroy(gameObject);
-					scoreManager.currentScore += 30;
+					ScoreManager.Instance.currentScore += 30;
 					break;
 				case AsteroidSize.SMALL:
-					spawner.Spawn(transform.position, AsteroidSize.TINY);
-					spawner.Spawn(transform.position, AsteroidSize.TINY);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.TINY);
+					AsteroidSpawner.Instance.Spawn(transform.position, AsteroidSize.TINY);
 					Destroy(gameObject);
-					scoreManager.currentScore += 20;
+					ScoreManager.Instance.currentScore += 20;
 					break;
 				case AsteroidSize.TINY: //If tiny size, then we just disappear
 					Destroy(gameObject);
-					scoreManager.currentScore += 10;
+					ScoreManager.Instance.currentScore += 10;
 					break;
 			}
 		}
